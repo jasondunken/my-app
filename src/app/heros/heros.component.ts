a6e9f7f4a7273dcc672589afadf2961a406f37c1
PDunken <43407700+jasondunken@users.noreply.github.com>
2018-10-19 16:13:19 -0400

    fixed hero detail display/removed initial test hero

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -9,10 +9,12 @@ import { HEROS } from '../mock-heros';
 })
 export class HerosComponent implements OnInit {
   heros = HEROS;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   constructor() { }
 
   ngOnInit() {
